8c559cf5de488597f6d33fad0e14096af17b7f18
ontaku1209 <r116315@g.kobe-kosen.ac.jp>
2019-09-17 16:25:41 +0900

    確認用

---
+++ unity/Assets/Scripts/GUI/Master.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Master : MonoBehaviour
+{
+    public GameObject Menu;
+    public static bool MenuOn = true;
+    private bool flag = false;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        // Eキー押しっぱなしでも連続で切り替わらないように
+        if (Input.GetKey(KeyCode.E) && flag == true)
+            return;
+
+        flag = false;
+
+        // メニュー非表示
+        if (Input.GetKey(KeyCode.E) && MenuOn == true)
+        {
+            Menu.SetActive(false);
+            MenuOn = false;
+            flag = true;
+            return;
+        }
+        // メニュー表示
+        if (Input.GetKey(KeyCode.E) && MenuOn == false)
+        {
+            Menu.SetActive(true);
+            MenuOn = true;
+            flag = true;
+            return;
+        }
+        /* メニューを開くとカーソル表示
+        if (MenuOn == true)
+            Cursor.visible = true;
+        else
+            Cursor.visible = false;
+            */
+    }
+
+}
