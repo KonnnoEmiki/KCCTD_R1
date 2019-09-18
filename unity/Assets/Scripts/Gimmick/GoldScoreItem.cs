b1b37dc75dcf2ec2a0ea8d3aafefc16c3824a230
KonnoEmiki <r116321@g.kobe-kosen.ac.jp>
2019-09-18 18:49:57 +0900

    アイテムの共有なくした

--- unity/Assets/Scripts/Gimmick/GoldScoreItem.cs
+++ unity/Assets/Scripts/Gimmick/GoldScoreItem.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GoldScoreItem : MonoBehaviour
+public class GoldScoreItem : MonobitEngine.MonoBehaviour
 {
     // トリガーとの接触時に呼ばれるコールバック
     void OnTriggerEnter(Collider hit)
@@ -10,7 +10,8 @@ public class GoldScoreItem : MonoBehaviour
         // 接触対象はPlayerタグですか？
         if (hit.CompareTag("Player") && NetworkGUI.gs == true)
         {
-            ScoreCounter.scoreflag = 4;
+            if (monobitView.isMine == true)
+                ScoreCounter.scoreflag = 4;
             // このコンポーネントを持つGameObjectを破棄する
             Destroy(gameObject);
         }
